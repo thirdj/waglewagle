2fa521183a4b687ed0f0b9e6d883d776a93958c3
Choi, Jaehee <jaehee-choi@rsupport.com>
2016-10-19 20:48:52 +0900

    #added connected user couting

--- src/index.js
+++ src/index.js
@@ -8,123 +8,58 @@ const dbRef = database.ref();
 
 // Get a key for a new Post.
 const newMsgKey = dbRef.child('wagle/messages').push().key;
-// const users = dbRef.child('wagle/users');
 
-// const randomColor = Math.floor(Math.random() * colors.length);
 const creation = moment().format('YYYY-MM-DD HH:mm:ss');
 const ucolor = colors[getColor()];
-const uname = unnamed();
-
-function getColor() {
-  return Math.floor(Math.random() * colors.length);
-}
-
-function joinUser() {
-  // dbRef.child('wagle/users').on('child_added', chat => {
-  //   console.info('wagle/users  child_added');
-  //   const chatData = chat.val();
-  //   let data = {};
-  //   // console.log('Object.keys(chatData)  ', Object.keys(chatData)[0]);
-  //   // console.log('newMsgKey ', newMsgKey);
-  //   // console.log('Object.keys(chatData)[0] ', Object.keys(chatData)[0]);
-  //   if (newMsgKey === Object.keys(chatData)[0]) {
-  //     console.log('users child_added same');
-  //     data = setAppendMsgData(chatData, newMsgKey);
-  //     appendMsg(data);
-  //   }
-  //   // console.log('users child_added chat  ', chatVal);
-  // });
-  const msg = '입장 했습니다.';
-  const uid = uname;
-  const msgData = { uid, msg, creation, ucolor };
-  const updates = {};
-
-  updates[`/wagle/users/${uname}/${newMsgKey}`] = msgData;
-  updates[`/wagle/messages/${newMsgKey}/${getCurrentTime().unix}`] = msgData;
-  // users.once('value', snapshot => {
-  //   console.log('[writeNewMessage] user.once');
-  //   if (!snapshot.hasChild(uid)) {
-  //     console.log('[writeNewMessage] not uid');
-  //     updates[`/wagle/users/${uid}/${newMsgKey}`] = msgData;
-  //   } else {
-  //     console.log('[writeNewMessage] That user already exists');
-  //   }
-  // });
-  // console.log('[writeNewMessage] before return');
-  return database.ref().update(updates);
-}
-
-joinUser();
+const uid = unnamed();
 
 // "value", "child_added", "child_removed", "child_changed", or "child_moved".
-dbRef.child('wagle/users').on('child_removed', chat => {
-  const chatVal = chat.val();
-  console.log('users child_removed chat  ', chatVal);
+dbRef.child('wagle/users').on('child_removed', snap => {
+  const snapVal = snap.val();
+  console.log('users child_removed snap  ', snapVal);
 });
 
-dbRef.child('wagle/users').on('child_added', chat => {
+// connected user count
+dbRef.child('wagle/users').on('value', (snap) => {
+  // const posts = snap.val();
+  // const keys = Object.keys(posts);
+  const count = snap.numChildren();
+
+  $('#join-counting').text((count > 1) ? count : '');
+});
+
+dbRef.child('wagle/users').on('child_added', snap => {
   // console.info('wagle/users  child_added');
-  const chatData = chat.val();
+  const snapData = snap.val();
   let data = {};
-  // console.log('Object.keys(chatData)  ', Object.keys(chatData)[0]);
-  // console.log('newMsgKey ', newMsgKey);
-  // console.log('Object.keys(chatData)[0] ', Object.keys(chatData)[0]);
-  if (newMsgKey === Object.keys(chatData)[0]) {
-    // console.log('users child_added same');
-    data = setAppendMsgData(chatData, newMsgKey);
+
+  if (newMsgKey === Object.keys(snapData)[0]) {
+    data = setAppendMsgData(snapData, newMsgKey);
   }
-  appendMsg(data);
-  // console.log('users child_added chat  ', chatVal);
-});
-/*
-dbRef.child('wagle/messages').on('child_added', chat => {
-  console.info('wagle/messages  child_added');
-  const chatData = chat.val();
-  // console.log('111 chatData  ', chatData);
-  let data = {};
-  for (const key in chatData) {
-    console.log('222 chatData  ', chatData[key]);
-    console.log('chatData[key].uid ', chatData[key].uid);
-    console.log('uname ', uname);
-    if (uname === chatData[key].uid) {
-      data = setAppendMsgData(chatData, key);
-      appendMsg(data);
-    }
-  }
-  // console.log('messages child_added chat  ', chatData);
-});
-*/
-dbRef.child('wagle/messages').on('child_changed', chat => {
-  // console.info('wagle/messages  child_changed');
-  const chatData = chat.val();
-  // console.log('111 chatData  ', chatData);
-  let data = {};
-  for (const key in chatData) {
-    // console.log('222 chatData  ', chatData[key]);
-    data = setAppendMsgData(chatData, key);
-  }
-  // console.log('messages child_changed chat  ', chatData);
-  appendMsg(data);
+  appendMsgTemplate(data);
 });
 
-function setAppendMsgData(data, key) {
-  // console.log('setAppendMsgData', data, key);
-  return {
-    uid: data[key].uid,
-    msg: data[key].msg,
-    ucolor: data[key].ucolor,
-    creation: data[key].creation
-  };
-}
+dbRef.child('wagle/messages').on('child_changed', snap => {
+  // console.info('wagle/messages  child_changed');
+  const snapData = snap.val();
+  let data = {};
+  for (const key in snapData) {
+    data = setAppendMsgData(snapData, key);
+  }
+  appendMsgTemplate(data);
+});
+
 
 $(() => {
   const wagleInputMessage = $('#wagle-input');
 
+  joinRoom();
+
+  wagleInputMessage.val('').focus();
+
   $('main, footer').on('click', () => {
     wagleInputMessage.focus();
   });
-
-  wagleInputMessage.val('').focus();
 
   $('#wagle-form').on('submit', e => {
     e.preventDefault();
@@ -141,10 +76,45 @@ $(() => {
 
     wagleInputMessage.val('').focus();
   });
+
+  $(window).on('unload', () => {
+    dbRef.child('wagle/users').child(uid).remove();
+    dbRef.child('wagle/messages').child(newMsgKey).remove();
+  });
 });
 
-function appendMsg(data) {
-  // console.log('appendMsg ', data);
+
+/*
+ * functions
+ *
+ */
+function getColor() {
+  return Math.floor(Math.random() * colors.length);
+}
+
+function joinRoom() {
+  const msg = '입장 했습니다.';
+  const msgData = { uid, msg, creation, ucolor };
+  const updates = {};
+
+  updates[`/wagle/users/${uid}/${newMsgKey}`] = msgData;
+  updates[`/wagle/messages/${newMsgKey}/${getCurrentTime().unix}`] = msgData;
+
+  return database.ref().update(updates);
+}
+
+function setAppendMsgData(data, key) {
+  // console.log('setAppendMsgData', data, key);
+  return {
+    uid: data[key].uid,
+    msg: data[key].msg,
+    ucolor: data[key].ucolor,
+    creation: data[key].creation
+  };
+}
+
+function appendMsgTemplate(data) {
+  // console.log('appendMsgTemplate ', data);
   if (Object.keys(data).length && data.constructor === Object) {
     const time = moment().format('HH:mm');
     $('#messages').append(
@@ -160,14 +130,14 @@ function appendMsg(data) {
   }
 }
 
-function writeNewMessage(msg, timestamp, uid = uname) {
+function writeNewMessage(msg, timestamp, uuid) {
   // console.info('start [writeNewMessage]');
   const msgData = { uid, msg, creation, ucolor };
   const updates = {};
 
   updates[`/wagle/messages/${newMsgKey}/${timestamp}`] = msgData;
-  updates[`/wagle/users/${uid}/${newMsgKey}`] = msgData;
-  // console.log('[writeNewMessage] before return');
+  updates[`/wagle/users/${uuid}/${newMsgKey}`] = msgData;
+
   return database.ref().update(updates);
 }
 
